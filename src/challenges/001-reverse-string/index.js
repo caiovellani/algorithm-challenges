8733dcea59701d5184403aeacff33225a62fb94e
Caio Vellani <caiovellani@outlook.com>
2024-12-10 22:15:50 -0300

    challenge 001 solved

--- src/challenges/001-reverse-string/index.js
+++ src/challenges/001-reverse-string/index.js
@@ -6,3 +6,9 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(string) {
+	return string.split("").reverse().join("");
+}
+
+module.exports = reverse;
